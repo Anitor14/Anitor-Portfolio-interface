5b424a32ed354db97b2c516293b115cf274c122e
Anitor <anitorabraham@gmail.com>
2022-10-21 21:02:42 +0100

    i changed home to home food

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ window.addEventListener("scroll", () => {
   if (window.pageYOffset > 80) {
     navbar.classList.add("navbar-fixed");
   } else {
-    navbar.classList.remove("navbar-fixed");
+    navbar.classList.remove("navbar-fixed");  
   }
 });
 
